88ac4ed05083867f90aa39d817ac40d3b98eb93d
Nihaal-Warraich <137639803+Nihaal-Warraich@users.noreply.github.com>
2025-10-24 21:29:06 -0400

    Feat/uc 014 colorscheme (#17)
    * Added color palette and readme file, also wrapped main
    
    * Added color palette and readme file, also wrapped main
    
    * added UC017 as well, works closely with UC014
    
    * saving changes
    
    * wrapped main with theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { router } from "./router.tsx";
 import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./theme/theme.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <RouterProvider router={router} />
-    </>
+    </ThemeProvider>
   </StrictMode>
 );
